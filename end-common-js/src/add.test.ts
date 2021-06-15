91059bd3d7ae4918802c9a9ac7a8f707907ce4df
k2snowman69 <rickyp@snowcoders.com>
2021-06-15 16:03:35 -0700

    Add async tests to show https://github.com/facebook/jest/issues/11563

--- end-common-js/src/add.test.ts
+++ end-common-js/src/add.test.ts
@@ -11,6 +11,11 @@ describe("add", () => {
     expect(value).toBe(6);
   });
 
+  it("should add two values (async)", async () => {
+    let value = await Utils.addTwice("1", "2");
+    expect(value).toBe(6);
+  });
+
   it("should throw exception if invalid value provided", () => {
     expect(() => {
       Utils.addTwice({}, "2");
